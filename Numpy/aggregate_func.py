9bc46b590a0d4a850f91fcf902ab119c76a41cfb
Saurabh Giri <81623624+saurabh9136@users.noreply.github.com>
2025-02-04 19:33:22 +0530

    Update aggregate_func.py

--- Numpy/aggregate_func.py
+++ Numpy/aggregate_func.py
@@ -3,4 +3,29 @@ import numpy as np
 
 a = np.array([7, 3, 1, 8, 4, 2, 5, 6])
 
-# Sum of the array
+# Basic aggregate functions
+'''
+print(np. sum(a))
+print (np.min(a))
+print(np.max(a))
+print(np.size(a))
+print (np.mean(a))
+print(np.cumsum(a))
+print(np.cumprod(a))
+
+'''
+
+
+z = [100,150,199, 200, 250, 130] 
+b = [10,50,30,40,30,10]
+
+price = np.array(z) 
+quantity = np.array(b)
+
+#print(price, "\n", quantity)
+
+# let calculate the total price
+
+totalSum = np.cumprod([price, quantity], axis=0)
+
+print(totalSum[1].sum())
